6e162ca220d829e6c2e217517148f6d68e4a478f
givara.ebo@gmail.com <givara.ebo@gmail.com>
2025-04-13 20:08:02 +0200

    update

--- test.py
+++ test.py
@@ -1,33 +1,60 @@
-import pandas as pd
 
-# Beispiel-Daten
-df = pd.DataFrame({
-    'A': [1, 2, 3],
-    'B': ['x', 'y', 'z'],
-    'C': [10, 20, 30],
-    'D': [100, 200, 300]
-})
+# Person class mit setter und getter mit toJson() und fromJson() Methoden
+class Person:
 
-# Neue Zeile, die hinzugefügt werden soll
-new_row = {'A': 4, 'B': 'w'}
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
 
-# Zeile hinzufügen
-df = df._append(new_row, ignore_index=True)
+    @property
+    def name(self):
+        return self._name
 
-liste = ["x", "y", "z"]
+    @name.setter
+    def name(self, value):
+        self._name = value
 
-liste_2 = [1, 2]
+    @property
+    def age(self):
+        return self._age
 
-print(df)
-output = pd.DataFrame()
-for buchstabe in liste:
-    for zahl in liste_2:
-        df_2 = df.loc[(df["A"] == zahl)&(df["B"].str.strip() == buchstabe), ["C", "D"]]
-        if df_2.empty:
-            continue
-        label = buchstabe+" " + str(zahl)
-        df_2["label"] = label
-        output = output._append(df_2, ignore_index=True)
-        print(output)
+    @age.setter
+    def age(self, value):
+        if value < 0:
+            raise ValueError("Age cannot be negative")
+        self._age = value
+
+    def to_json(self):
+        return {"name": self._name, "age": self._age}
+
+    @classmethod
+    def from_json(cls, json_data):
+        return cls(json_data["name"], json_data["age"])
 
 
+
+max = Person("Max", 25)
+
+print(max.to_json())
+
+test = {'name': 'Marc', 'age': 23}
+
+
+
+
+
+
+marc = Person.from_json(test)
+
+marc.age = max.age+2
+
+
+
+
+
+
+print(marc.to_json())
+
+# erkläre serialization und deserialization
+# Serialization ist der Prozess, bei dem ein Objekt in ein Format umgewandelt wird, das gespeichert oder übertragen werden kann.
+# Deserialization ist der Prozess, bei dem die gespeicherten oder übertragenen Daten zurück in ein Objekt umgewandelt werden.
